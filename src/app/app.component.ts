b7f5c44cc3a0861bbbeeff73fdcf9c7d9bc1517d
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-12-25 06:34:34 +0100

    Creating a WishItem Class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  items = [
+    new WishItem("Learn Angular"),
+    new WishItem("Get Coffee", true),
+    new WishItem("Find Grass that cuts itself")
+  ];
   title = 'angular-wish-list';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// App Main TypeScript File
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
